5a5f235e2e6dfbea6b25b7fbf084f50a1b7e47c3
buichidunggithub <55236614+buichidunggithub@users.noreply.github.com>
2020-09-21 14:01:08 +0700

    Add files via upload

---
+++ 1712364_1712584_1712588_1712615_1712641_BT4/1712364_1712584_1712588_1712615_1712641_BT4/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1712364_1712584_1712588_1712615_1712641_BT4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
